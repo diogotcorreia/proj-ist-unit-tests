a97e841a3bbb06366e283a6530f3d4df5d59e1aa
Diogo Correia <me@diogotc.com>
2021-10-24 01:17:52 +0100

    Add some official tests (read after import) (PO 2021)

--- po/2021-2022/po-p1/tests/ggctests/ReadAfterImportTest.java
+++ po/2021-2022/po-p1/tests/ggctests/ReadAfterImportTest.java
@@ -39,12 +39,56 @@ public class ReadAfterImportTest {
 
             assertEquals(getTextFromFile("expected/test" + testId + "-products.output"), this.interaction.getResult());
         }
+
+        @Test
+        void listBatches() {
+            this.interaction.addMenuOptions(5, 2);
+
+            this.runApp();
+
+            assertEquals(getTextFromFile("expected/test" + testId + "-batches.output"), this.interaction.getResult());
+        }
     }
 
     @Nested
     public class ExampleInputFromWikiTest extends GenericReadAfterImportTest {
         public ExampleInputFromWikiTest() {
             super("001");
+        }
+
+        void listBatches() {}
+    }
+
+    /**
+    Corresponds to test A-01-02-M-ok
+     */
+    @Nested
+    public class InputWithOnlyPartnersTest extends GenericReadAfterImportTest {
+        public InputWithOnlyPartnersTest() {
+            super("002");
+        }
+
+        void listProducts() {}
+        void listBatches() {}
+    }
+
+    /**
+    Corresponds to test A-01-04-M-ok
+     */
+    @Nested
+    public class InputWithPartnersAndSimpleProducts1Test extends GenericReadAfterImportTest {
+        public InputWithPartnersAndSimpleProducts1Test() {
+            super("003");
+        }
+    }
+
+    /**
+    Corresponds to test A-03-02-M-ok
+     */
+    @Nested
+    public class InputWithPartnersAndSimpleProducts2Test extends GenericReadAfterImportTest {
+        public InputWithPartnersAndSimpleProducts2Test() {
+            super("004");
         }
     }
 
