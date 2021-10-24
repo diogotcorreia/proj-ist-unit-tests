a4bffbb2efa52dc1e727c05565d49cefb21f3800
Diogo Correia <me@diogotc.com>
2021-10-24 23:53:39 +0100

    Add more official tests (PO 2021)

--- po/2021-2022/po-p1/tests/ggctests/ReadAfterImportTest.java
+++ po/2021-2022/po-p1/tests/ggctests/ReadAfterImportTest.java
@@ -83,13 +83,86 @@ public class ReadAfterImportTest {
     }
 
     /**
-    Corresponds to test A-03-02-M-ok
+    Corresponds to tests A-03-02-M-ok and A-04-02-M-ok
      */
     @Nested
     public class InputWithPartnersAndSimpleProducts2Test extends GenericReadAfterImportTest {
         public InputWithPartnersAndSimpleProducts2Test() {
             super("004");
         }
+    }
+
+    /**
+    Corresponds to test A-03-03-M-ok
+     */
+    @Nested
+    public class InputWithPartnersAndSimpleProducts3Test extends GenericReadAfterImportTest {
+        public InputWithPartnersAndSimpleProducts3Test() {
+            super("005");
+        }
+    }
+
+    /**
+    Corresponds to test A-04-03-M-ok
+     */
+    @Nested
+    public class InputWithProductWithMultipleBatches1Test extends GenericReadAfterImportTest {
+        public InputWithProductWithMultipleBatches1Test() {
+            super("006");
+        }
+    }
+
+    /**
+    Corresponds to test A-04-04-M-ok
+     */
+    @Nested
+    public class InputWithProductWithMultipleBatches2Test extends GenericReadAfterImportTest {
+        public InputWithProductWithMultipleBatches2Test() {
+            super("007");
+        }
+    }
+
+    /**
+    Corresponds to test A-07-01-M-ok
+     */
+    @Nested
+    public class ShowPartnerByKeyTest extends GenericReadAfterImportTest {
+        public ShowPartnerByKeyTest() {
+            super("008");
+        }
+
+        @Test
+        void getPartnerByKey1() {
+            this.interaction.addMenuOptions(6, 1);
+            this.interaction.addFieldValues("M1");
+
+            this.runApp();
+
+            assertEquals("M1|Rohit Figueiredo|New Delhi, India|NORMAL|0|0|0|0", this.interaction.getResult());
+        }
+
+        @Test
+        void getPartnerByKey2() {
+            this.interaction.addMenuOptions(6, 1);
+            this.interaction.addFieldValues("P1");
+
+            this.runApp();
+
+            assertEquals("P1|Lages do Chão|Lisboa, Portugal|NORMAL|0|0|0|0", this.interaction.getResult());
+        }
+
+        @Test
+        void getPartnerByKey3() {
+            this.interaction.addMenuOptions(6, 1);
+            this.interaction.addFieldValues("W2");
+
+            this.runApp();
+
+            assertEquals("W2|Pedraria Fonseca|Oeiras, Portugal|NORMAL|0|0|0|0", this.interaction.getResult());
+        }
+
+        void listProducts() {}
+        void listBatches() {}
     }
 
 }
