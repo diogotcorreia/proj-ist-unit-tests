e752f95db71d78da0cea8f7eb4c0364f706ad70c
Diogo Correia <me@diogotc.com>
2021-11-11 19:30:47 +0000

    Add lookup payments by partner official tests (PO 2021)

---
+++ po/2021-2022/po-p1/tests/ggctests/LookupPaymentsByPartnerTest.java
@@ -0,0 +1,63 @@
+package ggctests;
+
+import ggctests.utils.PoUILibTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LookupPaymentsByPartnerTest extends PoUILibTest {
+
+    public LookupPaymentsByPartnerTest() {
+      super(true);
+    }
+
+    @Override
+    protected void setupWarehouseManager() {
+    }
+
+    @Test
+    @DisplayName("A-14-01-M-ok - Mostrar parceiro não existente e parceiros sem transacções")
+    void showUnknownPartnerAndEmptyPartner() {
+        loadFromInputFile("test027.input");
+        this.interaction.addMenuOptions(8, 2, 2);
+        this.interaction.addFieldValues("nãoexiste", "M1");
+
+        this.runApp();
+
+        assertThrownCommandException("UnknownPartnerKeyException", "O parceiro 'nãoexiste' não existe.");
+        assertNoMoreExceptions();
+        assertEquals("", this.interaction.getResult());
+    }
+
+    @Test
+    @DisplayName("A-14-02-M-ok - Mostrar parceiro só com compras")
+    void showPartnerWithAcquisitionsOnly() {
+        loadFromInputFile("test027.input");
+        this.interaction.addMenuOptions(7, 4, 4, 0, 7, 1, 0);
+        this.interaction.addFieldValues("M1", "SAL", "10", "10", "M1", "VIDRO", "10", "20", "0");
+        this.interaction.addMenuOptions(8, 2);
+        this.interaction.addFieldValues("M1");
+
+        this.runApp();
+
+        assertNoMoreExceptions();
+        assertEquals("COMPRA|0|M1|SAL|10|100|0", this.interaction.getResult());
+    }
+
+    @Test
+    @DisplayName("A-14-03-M-ok - Mostrar parceiro só com vendas não pagas")
+    void showPartnerWithOnlyUnpaidSales() {
+        loadFromInputFile("test027.input");
+        this.interaction.addMenuOptions(7, 3, 3, 0);
+        this.interaction.addFieldValues("M1", "10", "ROLHA", "10", "M1", "5", "VIDRO", "25");
+        this.interaction.addMenuOptions(8, 2);
+        this.interaction.addFieldValues("M1");
+
+        this.runApp();
+
+        assertNoMoreExceptions();
+        assertEquals("", this.interaction.getResult());
+    }
+
+}
