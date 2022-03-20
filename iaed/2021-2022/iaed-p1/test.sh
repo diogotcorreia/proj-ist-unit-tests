edff04a0d38aa03d3ce25a8bf9a6b0c941a1f601
Francisco Salgueiro <92053465+franciscoBSalgueiro@users.noreply.github.com>
2022-03-20 13:29:06 +0000

    Add IAED 2021-2022 tests and improve script (#55)

--- iaed/2021-2022/iaed-p1/test.sh
+++ iaed/2021-2022/iaed-p1/test.sh
@@ -3,19 +3,23 @@
 #!/bin/bash
 
 if [[ $# -lt 2 ]]; then
-	echo "usage: $0 <relative_path_to_executable> <relative_path_to_tests_dir> <flags>"
-	echo "-v flag to display diff in the terminal"
-	echo "-c flag to remove generated result files"
-	echo "note: do not use the -c option without -v"
-	exit
+    echo "usage: $0 <relative_path_to_executable> <relative_path_to_tests_dir> <flags>"
+    echo "-v flag to display diff in the terminal"
+    echo "-c flag to remove generated result files"
+    exit
 fi
 
 bin="${1}"
 tests="${2}"
 
+if [ "$3" == "-c" ]; then
+    rm $PWD/$tests/*result
+    exit
+fi
+
 for infile in $PWD/$tests/*.in; do
     basename=${infile%.*}
-	($PWD/$bin) < $infile > $basename.result
+    ($PWD/$bin) < $infile > $basename.result
     cmp --silent $basename.result $basename.out || echo "$basename TEST FAILED" 
     if [ "$3" == "-v" ]; then
         if ! command -v colordiff &> /dev/null; then
@@ -30,12 +34,7 @@ if [ "$3" == "-v" ]; then
     if ! command -v colordiff &> /dev/null
     then
         echo ""
-        echo "NOTA: Instala colordiff para ver as diferenÃ§as a cores"
+        echo "NOTA: Instala colordiff para ver as diferenças a cores"
         exit
     fi
 fi
-
-if [ "$4" == "-c" ]; then
-	rm $PWD/$tests/*result
-fi
-
