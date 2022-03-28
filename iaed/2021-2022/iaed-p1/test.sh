3f282e0844a0ca54322b9489698cf4140b9ef399
André Breda <andrebreda@tecnico.ulisboa.pt>
2022-03-28 15:47:30 +0100

    Avoid using $PWD, modularize diffing

--- iaed/2021-2022/iaed-p1/test.sh
+++ iaed/2021-2022/iaed-p1/test.sh
@@ -8,7 +8,11 @@ usage() {
     echo "-h help - shows this message"
 }
 
-DIFF=true # no-op successful command
+silent_diff() {
+    cmp --silent "$1" "$2"
+}
+
+DIFF=silent_diff
 
 while getopts ":vch" OPTION; do
     case "$OPTION" in
@@ -34,28 +38,42 @@ if [[ $# -lt 2 ]]; then
     exit 1
 fi
 
-bin="${1}"
-tests="${2}"
+bin="$1"
+tests="$2"
 
+if [ ! -d "$tests" ]; then
+    echo Test dir '"'$tests'"' is not a directory
+    exit 1
+elif ! ls $tests/*.in &> /dev/null; then
+    echo Test dir '"'$tests'"' does not contain any tests
+    exit 1
+fi
+
+# Try to clean before finishing checks to allow cleaning without compiling anything
 if [[ "$MODE" == "clean" ]]; then
-    rm "$PWD/$tests/"*result
+    rm "$tests/"*result
     exit 0
 fi
 
-for infile in $PWD/$tests/*.in; do
-    basename=${infile%.*}
-    ($PWD/$bin) < $infile > $basename.result
-    cmp --silent $basename.result $basename.out || echo "$basename TEST FAILED" 
-    if [ "$3" == "-v" ]; then
-        if ! command -v colordiff &> /dev/null; then
-            diff $basename.result $basename.out
-        else
-            colordiff $basename.result $basename.out        
-        fi
-    fi
+if [ ! -f "$bin" ]; then
+    echo '"'$bin'"' is not a file
+    exit 1
+elif [ ! -x "$bin" ]; then
+    echo '"'$bin'"' is not executable
+    exit 1
+fi
+
+for infile in "$tests/"*.in; do
+    test_name="$(basename -s .in "$infile")"
+    actual_output_file="$(dirname $infile)/${test_name}.result"
+    expected_output_file="$(dirname $infile)/${test_name}.out"
+
+    "$bin" < "$infile" > "$actual_output_file" && \
+        "$DIFF" "$expected_output_file" "$actual_output_file" || \
+        echo "TEST FAILED: $test_name"
 done
 
-if [[ "$DIFF" != "colordiff" ]]; then
+if [[ "$DIFF" != "colordiff" && "$DIFF" != "silent_diff" ]]; then
     echo ""
     echo "NOTA: Instala colordiff para ver as diferenças a cores"
 fi
